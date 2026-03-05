809035a8a81edc8c3448e2453eddfcdb14b12e11
Vansh <sutharvansh2202@gmail.com>
2026-03-05 17:30:18 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const bookRoutes = require("./routes/bookRoutes");
 const adminRoutes = require("./routes/admin");
